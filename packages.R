9e283b0f3064e2b4bfa843b79cbe1f804ce9c052
Joshua Longbottom <Joshua.Longbottom@lstmed.ac.uk>
2019-01-14 15:28:46 +0000

    Packages script
    Script to source a local version of each package, hosted within the "package" directory.

---
+++ packages.R
@@ -0,0 +1,7 @@
+# script to source packages from the git 'packages' directory
+library(pacman, lib.loc = 'packages')
+library(raster, lib.loc = 'packages')
+library(shiny, lib.loc = 'packages')
+library(RColorBrewer, lib.loc = 'packages')
+library(malariaAtlas, lib.loc = 'packages')
+library(shinydashboard, lib.loc = 'packages')
