f887cedb4477a1b3eb225e0a8f6449c3bbe4de89
Sean Tomlinson <sean.tomlinson@outlook.com>
2019-02-14 10:07:19 +0000

    added navigable sections for ahelp about etc

---
+++ about.md
@@ -0,0 +1,27 @@
+### Developer
+
+**SpatialEpiApp** has been developed by
+
+
+|     |
+| --- |
+|Paula Moraga |
+|Lancaster Medical School |
+|Lancaster University |
+|Lancaster, LA1 4YG, United Kingdom |
+|E-mail: p.e.moraga-serrano@lancaster.ac.uk |
+|URL: http://Paula-Moraga.github.io |
+|Twitter: @\_PaulaMoraga\_ |
+
+### Citation
+
+To cite **SpatialEpiApp** in publications use:
+
+Paula Moraga (2017), SpatialEpiApp: A Shiny web application for the analysis of spatial and spatio-temporal disease data. Spatial and Spatio-temporal Epidemiology, 23:47-57
+DOI: https://doi.org/10.1016/j.sste.2017.08.001
+
+
+<!-- Paula Moraga (2017). SpatialEpiApp: A Shiny web application for the analysis of spatial and spatio-temporal disease data. R package version 0.1, URL https://CRAN.R-project.org/package=SpatialEpiApp.
+-->
+
+
--- ui.R
+++ ui.R
@@ -9,76 +9,83 @@ africa$COUNTRY[africa$COUNTRY == "Tanzania"] <- "United Republic of Tanzania"
 
 
 # define a UI use a fluid bootstrap layout
-fluidPage(    
+
+navbarPage("Malaria Atlas Project - District comparison",
+           tabPanel("Application",
+                    fluidPage(    
+                      
+                      # set a margin for the checkbox
+                      tags$head(
+                        tags$style(
+                          HTML(".checkbox-inline { 
+                               margin-left: 0px;
+                               margin-right: 10px;
+                               }
+                               .checkbox-inline+.checkbox-inline {
+                               margin-left: 0px;
+                               margin-right: 10px;
+                               }"))),
   
-  # set a margin for the checkbox
-  tags$head(
-    tags$style(
-      HTML(".checkbox-inline { 
-           margin-left: 0px;
-           margin-right: 10px;
-           }
-           .checkbox-inline+.checkbox-inline {
-           margin-left: 0px;
-           margin-right: 10px;
-           }"))),
-  
-  # page title
-  titlePanel(HTML(paste("Malaria Atlas Project - District comparison", " ", " ", sep = "<br/>"))),
-  
-  actionButton(inputId = "help", label = "Help"),
-  actionButton(inputId = "about", label = "About"),
-  
-  # create a sidebar where the user can select a country, and districts (etc.)
-  # we may change this to a header once basic functionality is resolved
-  sidebarLayout(
-    
-    # sidebar panel for the inputs
-    sidebarPanel(
-      
-      # country of interest selection (only one country allowed at a time)
-      selectInput("country", "Select country of interest:",
-                  choices = unique(africa$COUNTRY),
-                  selected = "Benin"),
-      
-      # hover-over tooltip
-      bsTooltip(id = "country", 
-                title = "Please select the country of interest, available districts will update based on this selection.", 
-                placement = "right", trigger = "hover", options = list(container = "body")),
-      
-      # dynamic district selection
-      uiOutput("select_dist"),
-      
-      # hover-over tooltip
-      bsTooltip(id = "select_dist", 
-                title = "Please select the districts to feature within the comparison/ranking.", 
-                placement = "right", trigger = "hover", options = list(container = "body")),
-      
-      # dynamic district selection
-      uiOutput("select_raster"),
-      
-      # hover-over tooltip
-      bsTooltip(id = "select_raster", 
-                title = "Please select the rasters to compare.", 
-                placement = "right", trigger = "hover", options = list(container = "body"))),
-    
-      mainPanel(
-      
-      tabsetPanel(type = "tabs",
-                  tabPanel(title = "Selected country and districts", plotOutput("select_country")),
-                  tabPanel(title = "Selected district statistics - map", plotOutput("")),
-                  tabPanel(title = "Selected district statistics - ranking", plotOutput("")))
-    )
-    
-  ), 
-  
-  # event to observe re statistics/ranking generation
-  actionButton(inputId = "processStats", label = "Generate statistics"),
-  # tooltip for 'Generate statistics'
-  bsTooltip(id = "processStats", 
-            title = "Run generation of statistics and ranking system. This will produce results which feature in the tabs to the right.", 
-            placement = "right", trigger = "hover", options = list(container = "body")),
-  
-  # event to observe the generation of a summary report featuring stats outputs
-  actionButton(inputId = "genReport", label = "Generate a summary report")
-      )
+                      # page title
+                      #titlePanel(HTML(paste("Malaria Atlas Project - District comparison", " ", " ", sep = "<br/>"))),
+                      
+                      # create a sidebar where the user can select a country, and districts (etc.)
+                      # we may change this to a header once basic functionality is resolved
+                      sidebarLayout(
+                        
+                        # sidebar panel for the inputs
+                        sidebarPanel(
+                          
+                          # country of interest selection (only one country allowed at a time)
+                          selectInput("country", "Select country of interest:",
+                                      choices = unique(africa$COUNTRY),
+                                      selected = "Benin"),
+                          
+                          # hover-over tooltip
+                          bsTooltip(id = "country", 
+                                    title = "Please select the country of interest, available districts will update based on this selection.", 
+                                    placement = "right", trigger = "hover", options = list(container = "body")),
+                          
+                          # dynamic district selection
+                          uiOutput("select_dist"),
+                          
+                          # hover-over tooltip
+                          bsTooltip(id = "select_dist", 
+                                    title = "Please select the districts to feature within the comparison/ranking.", 
+                                    placement = "right", trigger = "hover", options = list(container = "body")),
+                          
+                          # dynamic district selection
+                          uiOutput("select_raster"),
+                          
+                          # hover-over tooltip
+                          bsTooltip(id = "select_raster", 
+                                    title = "Please select the rasters to compare.", 
+                                    placement = "right", trigger = "hover", options = list(container = "body"))),
+                        
+                        mainPanel(
+                          
+                          tabsetPanel(type = "tabs",
+                                      tabPanel(title = "Selected country and districts", plotOutput("select_country")),
+                                      tabPanel(title = "Selected district statistics - map", plotOutput("")),
+                                      tabPanel(title = "Selected district statistics - ranking", plotOutput("")))
+                        )
+                        
+                      ), 
+                      
+                      # event to observe re statistics/ranking generation
+                      actionButton(inputId = "processStats", label = "Generate statistics"),
+                      # tooltip for 'Generate statistics'
+                      bsTooltip(id = "processStats", 
+                                title = "Run generation of statistics and ranking system. This will produce results which feature in the tabs to the right.", 
+                                placement = "right", trigger = "hover", options = list(container = "body")),
+                      
+                      # event to observe the generation of a summary report featuring stats outputs
+                      actionButton(inputId = "genReport", label = "Generate a summary report")
+                          )),
+           tabPanel("Help",
+                    column(12, includeMarkdown("help.md"))),
+           tabPanel("About",
+                    column(12, includeMarkdown("about.md"))),
+           tabPanel("Methodology",
+                    column(12, includeMarkdown("methodology.md")))
+)
